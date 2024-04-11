265bd7a052298f530f08db9bb0ce192672d31a13
DmitriiButk <nudumka21@gmail.com>
2024-04-11 12:34:48 +0400

    Setting up the display of the admin panel.

--- diplom_django/backend/admin.py
+++ diplom_django/backend/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Shop, Category, Product, ProductInfo, Parameter, \
+    ProductParameter, Order, OrderItem, Contact
+
+
+admin.site.register(User)
+admin.site.register(Shop)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductInfo)
+admin.site.register(Parameter)
+admin.site.register(ProductParameter)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Contact)
